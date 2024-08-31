3999cadb61680b4d3380bf8b158e00b0e0d1603b
EricHackBloxFruit <lenhatthanh2022tv@gmail.com>
2024-08-31 20:26:03 +0700

    Update DomadicHubVietSub.lua

--- DomadicHubVietSub.lua
+++ DomadicHubVietSub.lua
@@ -28,7 +28,7 @@ ImageButton1.BorderSizePixel = 0
 ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
 ImageButton1.Size = UDim2.new(0, 50, 0, 50)
 ImageButton1.Draggable = true
-ImageButton1.Image = "http://www.roblox.com/asset/?id=106898136342049"
+ImageButton1.Image = "http://www.roblox.com/asset/?id=127248047905825"
 ImageButton1.MouseButton1Down:connect(function()
   game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
   game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
@@ -15967,13 +15967,13 @@ spawn(function()
 print("Domadic Hub Load Complete")
 
 game.StarterGui:SetCore("SendNotification", {
-      Icon = "http://www.roblox.com/asset/?id=106898136342049";
+      Icon = "http://www.roblox.com/asset/?id=127248047905825";
       Title = "Eric Hub VN", 
       Text = "Chào mừng đến Script!";
 })
 wait(3)
 game.StarterGui:SetCore("SendNotification", {
-      Icon = "http://www.roblox.com/asset/?id=106898136342049";
+      Icon = "http://www.roblox.com/asset/?id=127248047905825";
       Title = "Eric Hub VN", 
       Text = "Tạo Bởi Eric được Vietsub";
 })
